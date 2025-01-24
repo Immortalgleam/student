808b9fe6256d23f1540a93c4604b4a61d9e3c5c3
Immortalgleam <olesya.zaets@gmail.com>
2025-01-24 23:31:44 +0200

    stored all fio and logins to list

--- student.py
+++ student.py
@@ -3,16 +3,26 @@
 def main():
     msg = "hi hello again"
     print(msg) 
+
+    res = []
    
     with open("data.txt","r", encoding="utf-8") as file_:
         for line in file_:
             try:
                 fio, login = line.split(";")
+                fio = fio.strip()                
+                login = login.strip()
             
                 print(f"fio = {fio} login = {login}")
+                res.append({
+                    "fio": fio,
+                    "login": login,
+                })
             except ValueError as err:
                 print(f"Cannot parse file: {err}")
                 return
+
+    print(res)
 
 if __name__ == "__main__":
     main()
