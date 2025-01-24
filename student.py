dcc321e33976cecbb7486537b421eb62b5c76b02
Immortalgleam <olesya.zaets@gmail.com>
2025-01-24 16:20:59 +0200

    added exception handling when no ; in data.txt

--- student.py
+++ student.py
@@ -2,13 +2,17 @@
 
 def main():
     msg = "hi hello again"
-    print(msg)
+    print(msg) 
    
     with open("data.txt","r", encoding="utf-8") as file_:
         for line in file_:
-            fio, login = line.split(";")
+            try:
+                fio, login = line.split(";")
             
-            print(f"fio = {fio} login = {login}")
+                print(f"fio = {fio} login = {login}")
+            except ValueError as err:
+                print(f"Cannot parse file: {err}")
+                return
 
 if __name__ == "__main__":
     main()
