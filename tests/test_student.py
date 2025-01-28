0fd627bb436470e348f055b0da6f1760606014e3
Immortalgleam <olesya.zaets@gmail.com>
2025-01-28 12:45:59 +0200

    updated test_student.py

--- tests/test_student.py
+++ tests/test_student.py
@@ -9,10 +9,8 @@ def file_student(tmpdir):
     filename = "data.txt"
     filename = tmpdir.join(filename)
     with open(filename, "w") as file_:
-        file_.write("""
-        
-        Невеличка Проблема;Immortalgleam
-'Най Буде;anetto-2
+        file_.write("""Невеличка Проблема ; Immortalgleam
+Най Буде;anetto-2
 """)
     return filename
 
@@ -21,14 +19,17 @@ def file_student_wrong(tmpdir):
     filename = "data_wrong.txt"
     filename = tmpdir.join(filename)
     with open(filename, "w") as file_:
-        file_.write("""
-        Най Буде;anetto-2
+        file_.write("""Невеличка Проблема ; Immortalgleam
+Най Буде;anetto-2
 """)
     return filename
 
 def test_student(file_student):
     sh_ans = [{'fio': 'Невеличка Проблема', 'login': 'Immortalgleam'}, {'fio': 'Най Буде', 'login': 'anetto-2'}]
     ans = get_logins(file_student)
-    print(ans)
 
-    assert sh_ans == ans
+    assert ans == sh_ans
+
+def test_student_wrong(file_student_wrong):
+    with pytest.raises(RuntimeError):
+        ans = get_logins(file_student_wrong)
